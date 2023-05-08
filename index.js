6163ad3387615143c9c03adcc11d6161d8b5afa8
Savannah <skruse101@gmail.com>
2023-05-08 12:33:26 -0700

    javascript event listening lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
-}
+
+const btn = document.getElementById('button');
+
+function clickAlert() {
+    alert('i was clicked!')
+} 
+
+btn.addEventListener('click', clickAlert);
+} 
+
+addingEventListener()
